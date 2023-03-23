68dc9df4994adfb4548937e32b8d5d2d667c9e96
podlaski <live.m851@gmail.com>
2023-03-23 21:43:50 +0100

    Dodawanie danych do apki, oraz mapowanie ich

--- src/ListWrapper/ListItem/ListItem.js
+++ src/ListWrapper/ListItem/ListItem.js
@@ -1,16 +1,15 @@
 import React from "react";
-import danAbramovImage from '../../assets/images/danabramov.jpg'
 
 
 import './ListItem.css';
 
-const ListItem = () => (
+const ListItem = (props) => (
     <li className="listItem__wrapper">
-        <img alt="DAN" src={danAbramovImage} className="listItem__image" />
+        <img alt="DAN" src={props.image} className="listItem__image" />
         <div>
-            <h2 className="listItem__name">Dan Abramov</h2>
-            <p className="listItem__description">It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office’s files, machines and decorations.</p>
-            <button className="listItem__button">visit twitter page</button>
+            <h2 className="listItem__name">{props.name}</h2>
+            <p className="listItem__description">{props.description}</p>
+            <button className="listItem__button"><a href={props.twitterLink}>Visit page</a></button>
         </div>
     </li>
 )
--- src/ListWrapper/ListWrapper.js
+++ src/ListWrapper/ListWrapper.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 import ListItem from "./ListItem/ListItem"
+import { twitterAccounts } from "../data/twitterAccounts";
+
 
 import './ListWrapper.css'
 
+
 const ListWrapper = () => (
     <ul>
-        <ListItem />
+        {twitterAccounts.map((account) => (
+            <ListItem
+                image={account.image}
+                name={account.name}
+                description={account.description}
+                twitterLink={account.twitterLink}
+            />
+        ))}
     </ul>
 )
 
