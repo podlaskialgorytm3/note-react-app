019a7c80692651562756f274fc7e54b5aba24fe5
podlaski <live.m851@gmail.com>
2023-03-25 21:15:06 +0100

    Refaktoryzacja kodu, dodanie CSS Modules

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,22 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ListItem.module.scss";
 
+const ListItem = ({ image, name, description, twitterLink }) => (
+  <li className={styles.wrapper}>
+    <img src={image} className={styles.image} alt={name} />
+    <div>
+      <h2 className={styles.name}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <a
+        href={twitterLink}
+        target="_blank"
+        className={styles.button}
+        rel="noopener noreferrer"
+      >
+        visit twitter page
+      </a>
+    </div>
+  </li>
+);
 
-import './ListItem.css';
+ListItem.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  twitterLink: PropTypes.string.isRequired,
+};
 
-const ListItem = ({
-    image,
-    name,
-    description,
-    twitterLink
-}) => (
-    <li className="listItem__wrapper">
-        <img alt="DAN" src={image} className="listItem__image" />
-        <div>
-            <h2 className="listItem__name">{name || "The simple name of famous person"}</h2>
-            <p className="listItem__description">{description || "The simple text about description"}</p>
-            <a targer="_blank" rel="noreferrer" className="listItem__button" href={twitterLink || "Twitterlink to his/her profile"}>Visit page</a>
-        </div>
-    </li>
-)
+ListItem.defaultProps = {
+  description: "One of the React creators",
+};
 
 export default ListItem;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,17 +1,13 @@
 import React from "react";
+import ListItem from "./ListItem/ListItem";
+import styles from "./ListWrapper.module.scss";
 
-import ListItem from "./ListItem/ListItem"
-
-
-import './ListWrapper.css'
-
-
-const ListWrapper = (props) => (
-    <ul className="listWrapper__wrapper">
-        {props.items.map((account) => (
-            <ListItem key={account.name} {...account} />
-        ))}
-    </ul>
-)
+const ListWrapper = props => (
+  <ul className={styles.wrapper}>
+    {props.items.map(item => (
+      <ListItem key={item.name} {...item} />
+    ))}
+  </ul>
+);
 
 export default ListWrapper;
